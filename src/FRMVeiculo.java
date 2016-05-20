aff07f2780bc48c57aa3aff61c34d7b9d5c54a04
josue-pinto <josue_pinto@yahoo.com>
2016-05-19 23:51:17 -0300

    tela do cadastro colaborador commit

--- src/FRMVeiculo.java
+++ src/FRMVeiculo.java
@@ -32,6 +32,8 @@ public class FRMVeiculo extends javax.swing.JFrame {
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenuItem3 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
 
         jMenu3.setText("jMenu3");
@@ -52,10 +54,15 @@ public class FRMVeiculo extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jMenu1.setText("Cadastrar");
-        jMenu1.setActionCommand("Cadastrar");
 
         jMenuItem1.setText("Veículo");
         jMenu1.add(jMenuItem1);
+
+        jMenuItem2.setText("Colaborador");
+        jMenu1.add(jMenuItem2);
+
+        jMenuItem3.setText("Fornecedor");
+        jMenu1.add(jMenuItem3);
 
         jMenuBar1.add(jMenu1);
 
@@ -119,6 +126,8 @@ public class FRMVeiculo extends javax.swing.JFrame {
     private javax.swing.JMenu jMenu3;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
