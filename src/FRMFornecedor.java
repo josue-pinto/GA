06b468121d898c580f8d37af6d40f248440b3ca4
josue-pinto <josue_pinto@yahoo.com>
2016-05-21 11:59:49 -0300

    tela do cadastro fornecedor baseado no modelo commit

--- src/FRMFornecedor.java
+++ src/FRMFornecedor.java
@@ -8,12 +8,12 @@
  *
  * @author josue.junior
  */
-public class FRMCadColaborador extends javax.swing.JFrame {
+public class FRMFornecedor extends javax.swing.JFrame {
 
     /**
-     * Creates new form FRMCadColaborador
+     * Creates new form FRMFornecedor
      */
-    public FRMCadColaborador() {
+    public FRMFornecedor() {
         initComponents();
     }
 
@@ -152,20 +152,20 @@ public class FRMCadColaborador extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FRMCadColaborador.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FRMFornecedor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FRMCadColaborador.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FRMFornecedor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FRMCadColaborador.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FRMFornecedor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FRMCadColaborador.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FRMFornecedor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new FRMCadColaborador().setVisible(true);
+                new FRMFornecedor().setVisible(true);
             }
         });
     }
