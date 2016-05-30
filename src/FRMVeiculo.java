be174c3967062cf68114320d44e9b71df4d6e0e9
josue-pinto <josue_pinto@yahoo.com>
2016-05-30 19:27:23 -0300

    menu todos ok, inserindo no banco ok

--- src/FRMVeiculo.java
+++ src/FRMVeiculo.java
@@ -35,6 +35,7 @@ public class FRMVeiculo extends javax.swing.JFrame {
         jMenuItem1 = new javax.swing.JMenuItem();
         jMenuItem2 = new javax.swing.JMenuItem();
         jMenuItem3 = new javax.swing.JMenuItem();
+        jMenu4 = new javax.swing.JMenu();
         jMenu2 = new javax.swing.JMenu();
 
         jMenu3.setText("jMenu3");
@@ -76,6 +77,9 @@ public class FRMVeiculo extends javax.swing.JFrame {
         jMenu1.add(jMenuItem3);
 
         jMenuBar1.add(jMenu1);
+
+        jMenu4.setText("Sobre");
+        jMenuBar1.add(jMenu4);
 
         jMenu2.setText("Sair");
         jMenu2.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -165,6 +169,7 @@ public class FRMVeiculo extends javax.swing.JFrame {
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenu jMenu3;
+    private javax.swing.JMenu jMenu4;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
